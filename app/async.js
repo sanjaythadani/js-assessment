3292a1465fa8e97c4438c0753e93309d12a0bdf9
sanjay <sanjaythadani@gmail.com>
2013-05-12 01:14:28 -0500

    async done

--- app/async.js
+++ app/async.js
@@ -3,11 +3,21 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+    	return $.Deferred(function(dfd) {
+    		dfd.resolve(value);
+    	});
     },
 
     manipulateRemoteData : function(url) {
-
+    	return $.Deferred(function(dfd) {
+	    	$.ajax({
+	    		url: url
+	    	}).then(function(data) {
+	    		dfd.resolve(data.people.map(function(item) {
+	    			return item.name;
+	    		}).sort());
+	    	});
+    	});
     }
   };
 });
